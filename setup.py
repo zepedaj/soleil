6ffc3c3d317bc04471f11da0f9ae05cd393be8e6
Joaquin Zepeda <jazs@jzbook.local>
2024-04-18 11:27:02 -0700

    Changing version number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="soleil",
     # Use `./soleil_examples/setup.py` to install the soleil examples
     packages=find_packages(".", exclude=["tests", "data_for_tests", "soleil_examples"]),
-    version="0.1.0",
+    version="0.1.1",
     scripts=["scripts/solex"],
     install_requires=["jztools", "climax", "rich"] + test_requires,
     author="Joaquin Zepeda",
