96ce47f9f0c6378e5e70c502eb16c2199ceb46ee
Joaquin Zepeda <jazs@jzbook.local>
2024-04-18 11:27:50 -0700

    Adding version number to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2023, Joaquin Zepeda"
 author = "Joaquin Zepeda"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.0 alpha"
+release = "0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
