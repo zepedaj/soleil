0ef4c8e73273a4c6c53a6ffb3c76472b3542c546
Joaquin Zepeda <jazs@jzbook.local>
2024-04-18 10:56:01 -0700

    Adding readme.md / description

---
+++ README.md
@@ -0,0 +1,16 @@
+
+Soleil is a Python object configuration mechanism inspired by [Meta's Hydra](https://hydra.cc/). It's main goal is to provide a Python mechanism for machine learning researchers to assemble flexible experiments in a way that is **declarative**, **inheritable**, **automatically CLI-invokable** and **intuitively overridable**.
+
+* Install soleil using
+
+```
+pip install soleil
+```
+
+
+* See [Soleil's full documentation](https://soleil.readthedocs.io/en/latest/) for more information
+
+
+* Contribute at [Soleil's github page](https://github.com/zepedaj/soleil)
+
+
--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ from setuptools import setup, find_packages
 
 test_requires = ["freezegun"]
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="soleil",
     # Use `./soleil_examples/setup.py` to install the soleil examples
     packages=find_packages(".", exclude=["tests", "data_for_tests", "soleil_examples"]),
     version="0.1.0",
-    description="Soleil: Lucid Configurations",
     scripts=["scripts/solex"],
     install_requires=["jztools", "climax", "rich"] + test_requires,
     author="Joaquin Zepeda",
+    description="Soleil: Lucid Configurations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zepedaj/soleil",
 )
